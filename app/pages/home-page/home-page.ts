f9e6472bfc16df7dbd22be877bd99e6f0d5418ce
jimmyein <Ruiming@ruimings-mbp.guest.corp.microsoft.com>
2016-07-26 12:16:55 -0700

    update style for home-page: 1. Combine sleep and activity 2. color change 3. Workout list no longer scrollable

--- app/pages/home-page/home-page.ts
+++ app/pages/home-page/home-page.ts
@@ -15,19 +15,6 @@ export class HomePage {
   public activity;
   public sleep;
   public socialMetrics;
-
-  public lineChartData: Array<any> = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-
-  public lineChartLabels: Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartType: string = 'line';
-  public lineChartOptions: any = {
-    animation: false,
-    responsive: true
-  };
-
   public events = null;
   public showDetail = false;
 
